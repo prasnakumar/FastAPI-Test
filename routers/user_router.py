6c319c3f8a00a66b0796940248814e618f37c996
Prasanna Kumar N <Prasanna.Augmento10@gmail.com>
2024-05-22 10:54:49 +0530

    update db

--- routers/user_router.py
+++ routers/user_router.py
@@ -14,18 +14,18 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/users/", response_model=schemas.User)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+@router.post("/users/", response_model=schemas.UserDetailCreate)
+async def create_user(user: schemas.UserDetailCreate, db: Session = Depends(get_db)):
     return await user_service.create_user(db=db, user=user)
 
-@router.get("/users/{user_id}", response_model=schemas.User)
+@router.get("/users/{user_id}", response_model=schemas.UserDetailCreate)
 async def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = await user_service.get_user(db=db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
-@router.get("/users/", response_model=list[schemas.User])
+@router.get("/users/", response_model=list[schemas.UserDetailCreate])
 async def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     users = await user_service.get_users(db=db, skip=skip, limit=limit)
     return users
--- services/user_service.py
+++ services/user_service.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from app import models, schemas
 
-async def create_user(db: Session, user: schemas.UserCreate):
+async def create_user(db: Session, user: schemas.UserDetailCreate):
     db_user = models.User(**user.dict())
     db.add(db_user)
     await db.commit()
@@ -9,7 +9,7 @@ async def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 async def get_user(db: Session, user_id: int):
-    return await db.query(models.User).filter(models.User.id == user_id).first()
+    return await db.query(models.UserDetail).filter(models.User.id == user_id).first()
 
 async def get_users(db: Session, skip: int = 0, limit: int = 10):
-    return await db.query(models.User).offset(skip).limit(limit).all()
+    return await db.query(models.UserDetail).offset(skip).limit(limit).all()
